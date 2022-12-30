6f88bc0ebc36b19a0de28a2160be2b0c8464c684
Malorf <109592209+Malorf@users.noreply.github.com>
2022-12-30 16:15:27 +0100

    importing assets

---
+++ Assets/Langage.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Langage : MonoBehaviour
+{
+    public bool french = true;
+    public bool english = false;
+    public TextMeshProUGUI languages;
+    public TextMeshProUGUI versus;
+    public TextMeshProUGUI solo;
+    public TextMeshProUGUI txtFrench;
+    public TextMeshProUGUI txtEnglish;
+    public void TextLanguages ()
+    {
+        if (french == true)
+            {
+            languages.text = "Langues";
+            versus.text = "Contre";
+            solo.text = "Seul";
+            txtFrench.text = "Français";
+            txtEnglish.text = "Anglais";
+        }
+        else
+        {
+            languages.text = "Languages";
+            versus.text = "Versus";
+            solo.text = "Solo";
+            txtFrench.text = "French";
+            txtEnglish.text = "English";
+        }
+    }
+    public void FrenchLanguage ()
+    {
+        french = true;
+        TextLanguages();
+    }
+    public void EnglishLanguage()
+    {
+        french = false;
+        TextLanguages();
+    }
+    // Update is called once per frame
+    void Start()
+    {
+        TextLanguages();
+    }
+}
